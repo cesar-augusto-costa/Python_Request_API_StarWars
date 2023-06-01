373bed1859ce8a1841503317b956d9f1d00ed7d7
Cesar Costa <cesar.costa.cac.1993@gmail.com>
2023-06-01 15:28:05 -0300

    testing

--- src/data/usescases/starships_list_colector_test.py
+++ src/data/usescases/starships_list_colector_test.py
@@ -1,11 +1,21 @@
-from src.infra import SwapiApiConsumer
+from src.infra.test.swapi_api_consumer import SwapiApiConsumerSpy
 
 from .starships_list_colector import StarshipsListColector
 
 
 def test_list():
-    api_consumer = SwapiApiConsumer()
+    ''' testing list method '''
+
+    api_consumer = SwapiApiConsumerSpy()
     starships_list_colector = StarshipsListColector(api_consumer)
 
     page = 1
-    starships_list_colector.list(page)
+    response = starships_list_colector.list(page)
+
+    # print(api_consumer.get_starships_attributes)
+    assert api_consumer.get_starships_attributes == {'page': page}
+    assert isinstance(response, list)
+    assert 'id' in response[0]
+    assert 'MGLT' in response[0]
+
+    print(response)

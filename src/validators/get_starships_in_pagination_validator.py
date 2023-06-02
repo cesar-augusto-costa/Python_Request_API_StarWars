3ad6aece59c454b5d16922367d596efd27f18e36
Cesar Costa <cesar.costa.cac.1993@gmail.com>
2023-06-02 11:23:17 -0300

    validator with cerberus

---
+++ src/validators/get_starships_in_pagination_validator.py
@@ -0,0 +1,24 @@
+from cerberus import Validator
+
+
+def get_pagination_validator(request: any):
+    ''' pagination validator '''
+
+    query_param_validator = Validator({
+        'page': {
+            'type': 'string',
+            'allowed': ['1', '2', '3', '4'],
+            'required': True
+        }
+    })
+
+    response = query_param_validator.validate(request.query_params)
+    
+    # print(query_param_validator.errors)
+    # print(response)
+
+    if response is False:
+        raise Exception(query_param_validator.errors)
+    
+
+

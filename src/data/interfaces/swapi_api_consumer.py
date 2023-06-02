f877da258912d99d5c1c3793e14a62f94279c990
Cesar Costa <cesar.costa.cac.1993@gmail.com>
2023-06-02 16:41:40 -0300

    get_starships_information

--- src/data/interfaces/swapi_api_consumer.py
+++ src/data/interfaces/swapi_api_consumer.py
@@ -11,3 +11,9 @@ class SwapiApiConsumerInterface(ABC):
     def get_starships(self, page: int) -> Tuple[int, Type[Request], Dict]:
         ''' Must Implement '''
         raise Exception('Must implement get_starships')
+    
+    @abstractmethod
+    def get_starships_information(self, starship_id: int) -> Tuple[int, Type[Request], Dict]:
+        ''' Must Implement '''
+        raise Exception('Must implement get_starships_information')
+    

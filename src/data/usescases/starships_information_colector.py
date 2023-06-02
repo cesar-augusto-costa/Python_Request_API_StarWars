afddc9b766df59784d1c34a3090ebf2a13acef8c
Cesar Costa <cesar.costa.cac.1993@gmail.com>
2023-06-02 16:52:25 -0300

    starships_information_colector

---
+++ src/data/usescases/starships_information_colector.py
@@ -0,0 +1,35 @@
+from typing import Dict, List, Type
+
+from src.data.interfaces.swapi_api_consumer import SwapiApiConsumerInterface
+from src.domain.usecases import StarshipsListColectorInterface
+
+
+class StarshipsListColector(StarshipsListColectorInterface):
+    ''' StarshipsListColector usecase '''
+
+    def __init__(self, api_consumer: Type[SwapiApiConsumerInterface]) -> None:
+        self.__api_consumer = api_consumer
+
+    def list(self, page: int) -> List[Dict]:
+        api_response = self.__api_consumer.get_starships(page)
+        print(api_response)
+        starships_formated_list = self.__format_api_response(api_response.response['results'])
+        return starships_formated_list
+    
+    @classmethod
+    def __format_api_response(cls, results: List[Dict]):
+        starships_formated_list = []
+
+        for starship in results:
+            starships_formated_list.append(
+                {
+                    "id": starship['url'].split('/')[-2],
+                    "name": starship['name'],
+                    "model": starship['model'],
+                    "max_atmosphering_speed": starship['max_atmosphering_speed'],
+                    "hyperdrive_rating": starship['hyperdrive_rating'],
+                    "MGLT": starship['MGLT'],
+                }
+            )
+        
+        return starships_formated_list

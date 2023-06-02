bc5975337e107a7af2eb2c988690370238196886
Cesar Costa <cesar.costa.cac.1993@gmail.com>
2023-06-02 14:51:55 -0300

    update routes

--- src/main/routes/starships_routes.py
+++ src/main/routes/starships_routes.py
@@ -1,16 +1,31 @@
 from fastapi import APIRouter
 from fastapi import Request as RequestFastApi
+from fastapi.responses import JSONResponse
 
+from src.main.adapters.request_adapter import request_adapter
+from src.main.composers.get_starships_in_pagination_composer import \
+    get_starships_in_pagination_composer
 from src.validators.get_starships_in_pagination_validator import \
     get_pagination_validator
 
 starships_routes = APIRouter()
 
 @starships_routes.get('/api/starships/list')
-def get_starships_in_pagination(request: RequestFastApi):
+async def get_starships_in_pagination(request: RequestFastApi):
     ''' get_starships_in_pagination '''
 
+    get_pagination_validator(request)
+    controller = get_starships_in_pagination_composer()
 
-    print(request.query_params)
-    print(request.query_params['page'])
-    return { 'Ola': 'Mundo' }
+    response = await request_adapter(request, controller.handle)
+    
+    # print(request.query_params)
+    # print(request.query_params['page'])
+    # return { 'Ola': 'Mundo' }
+
+    return JSONResponse(
+        status_code=response['status_code'],
+        # content=response['data']
+        content={'data': response['data']}
+
+    )

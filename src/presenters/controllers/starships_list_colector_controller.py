c57791b6880a11b85e975255a8d09f82d1570738
Cesar Costa <cesar.costa.cac.1993@gmail.com>
2023-06-02 14:53:04 -0300

    presenters

---
+++ src/presenters/controllers/starships_list_colector_controller.py
@@ -0,0 +1,20 @@
+from typing import Dict, Type
+
+from src.domain.usecases.starships_list_colector import \
+    StarshipsListColectorInterface
+
+
+class StarshipsListColectorController:
+    ''' Controller to List Starships '''
+
+    def __init__(self, starships_list_colector: Type[StarshipsListColectorInterface]) -> None:
+        self.__use_case = starships_list_colector
+    
+    def handle(self, http_request: Dict):
+        ''' Handler to List Colector '''
+
+        page = http_request['query_params']['page']
+        starships_list = self.__use_case.list(page)
+        http_response = {'status_code': 200, 'data': starships_list}
+
+        return http_response

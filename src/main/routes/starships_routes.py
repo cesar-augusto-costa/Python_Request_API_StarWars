10b334e615c92363f9e7949cf8bb255a56a2295d
Cesar Costa <cesar.costa.cac.1993@gmail.com>
2023-06-06 15:33:51 -0300

    updade routes

--- src/main/routes/starships_routes.py
+++ src/main/routes/starships_routes.py
@@ -5,7 +5,9 @@ from fastapi.responses import JSONResponse
 from src.main.adapters import request_adapter
 from src.main.composer import (get_starship_information_composer,
                                get_starships_in_pagination_composer)
-from src.validators import get_pagination_validator
+from src.presenters.errors import handle_errors
+from src.validators import (get_pagination_validator,
+                            get_starship_information_validator)
 
 starships_routes = APIRouter()
 
@@ -13,30 +15,38 @@ starships_routes = APIRouter()
 async def get_starships_in_pagination(request: RequestFastApi):
     ''' get_starships_in_pagination '''
 
-    get_pagination_validator(request)
+    response = None
     controller = get_starships_in_pagination_composer()
 
-    response = await request_adapter(request, controller.handle)
-    
+    try:
+        get_pagination_validator(request)
+        response = await request_adapter(request, controller.handle)
+    except Exception as error:
+        response = handle_errors(error)
+
     # print(request.query_params)
     # print(request.query_params['page'])
     # return { 'Ola': 'Mundo' }
 
     return JSONResponse(
         status_code=response['status_code'],
-        # content=response['data']
-        content={'data': response['data']}
-
+        content=response['data']
+        # content={'data': response['data']}
     )
 
 # Essa rota poderia ser com GET
 @starships_routes.post("/api/starships/information")
 async def get_starship_information(request: RequestFastApi):
     ''' get_starship_information '''
-    
+    ## 43
+    response = None
     controller = get_starship_information_composer()
 
-    response = await request_adapter(request, controller.handler)
+    try:
+        await get_starship_information_validator(request)
+        response = await request_adapter(request, controller.handler)
+    except Exception as error:
+        response = handle_errors(error)    
 
     return JSONResponse(
         status_code=response["status_code"],
